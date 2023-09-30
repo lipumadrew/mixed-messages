67c1e6db44a105ee650c80ec5e4cae4d7974610b
Andrew Lipuma <andrewlipuma@gmail.com>
2023-09-29 21:05:09 -0700

    Add pronoun generator with probability

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const dictionary = {
     adjectives: ["hungry", "sleepy", "ugly", "wonderful", "polite", "handsome", "talented", "awesome", "terrible", "dumb", "silly", "happy", "confused", "sour", "sweet", "salty", "determined", "fearless", "scary", "spooky", "hilarious", "rad", "cool", "beautiful", "haunted", "cursed"],
-    person: ["mother", "father", "sister", "brother", "grandma", "husband", "wife", "aunt", "uncle", "grandpa", "son", "daughter", "teacher", "student", "college student", "doctor", "police officer", "software engineer", "trick-or-treater", "violin teacher", "ghost", "witch", "zombie", "vampie", "werewolf"],
+    person: ["mother", "father", "sister", "brother", "grandma", "husband", "wife", "aunt", "uncle", "grandpa", "son", "daughter", "teacher", "student", "college student", "doctor", "police officer", "software engineer", "trick-or-treater", "violin teacher", "ghost", "witch", "wizard", "zombie", "vampie", "werewolf"],
     place: ["school", "workplace", "grocery store", "Safeway", "college", "hospital", "house", "farm", "tech startup", "nuclear power plant", "graveyard", "mausoleum", "spirit world", "jail", "sewer", "forest", "cave", "fortune teller", "corn maze", "secret passage", "underworld"],
     thing: ["carrot", "apple", "orange", "computer", "house", "rotisserie-chicken", "cake", "homework", "portfolio-project", "pumpkin", "potion", "poison apple", "spellbook", "jack-o-lantern", "broom", "cauldron", "tombstone", "eye of newt", "pentagram", "candy corn"],
     things: ["fruits", "vegetables", "legumes", "eyeballs", "cobwebs", "toenails", "voodoo dolls", "noodles", "flowers", "video games", "clothes", "groceries", "halloween decorations", "autumn leaves", "spiders", "snacks"],
@@ -11,8 +11,31 @@ const dictionary = {
     adverb: ["always", "never", "quickly", "slowly", "sometimes", "usually", "rarely"]
 };
 
+//I'm realizing that for this to work well, I would need the message template to be an array so I can index the correct word to generate a pronoun for
+const generatePronoun = (word) => {
+    let pronoun = "they";
+    if (word === "mother" || word === "grandma" || word === "sister" || word === "wife" || word === "aunt" || word === "daughter" || word === "witch") {
+        pronoun = "she";
+    } else if (word === "father" || word === "brother" || word === "husband" || word === "uncle" || word === "grandpa" || word === "son" || word === "wizard") {
+        pronoun = "he";
+    } else {
+        let fiftyFifty = (Math.floor(Math.random() * 2) == 1 );
+        if (fiftyFifty) {
+            pronoun = "he";
+        } else {
+            pronoun = "she";
+        }
+    }
+    //I created a 1 in 10 chance that the person uses a gender-neutral pronoun, regardless of the word
+    let neutralChance = (Math.floor(Math.random() * 10) + 1);
+    if (neutralChance == 1) {
+        pronoun = "they";
+    }
+    return pronoun;
+}
+
 const messageTemplates = [
-    "I sent my " + randomlySelectFromArray(dictionary.person) + " to the " + randomlySelectFromArray(dictionary.place) + " to get some " + randomlySelectFromArray(dictionary.things) + ".",
+    ["I sent my"]
 ];
 
 function randomlySelectFromArray(array) {
@@ -21,6 +44,5 @@ function randomlySelectFromArray(array) {
 
 
 
-console.log(randomlySelectFromArray(messageTemplates));
 
 console.log("The script has been run! :)");
