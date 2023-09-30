40261d2e33d8edcd0fb0cfce37554bfd9560aacd
Andrew Lipuma <andrewlipuma@gmail.com>
2023-09-29 22:00:27 -0700

    Add support for more versatile pronoun usage

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const dictionary = {
-    adjectives: ["hungry", "sleepy", "ugly", "wonderful", "polite", "handsome", "talented", "awesome", "terrible", "dumb", "silly", "happy", "confused", "sour", "sweet", "salty", "determined", "fearless", "scary", "spooky", "hilarious", "rad", "cool", "beautiful", "haunted", "cursed"],
+    adjectives: ["hungry", "sleepy", "ugly", "wonderful", "polite", "handsome", "ashamed", "talented", "awesome", "terrible", "dumb", "silly", "happy", "confused", "sour", "sweet", "salty", "determined", "fearless", "scary", "spooky", "hilarious", "rad", "cool", "beautiful", "haunted", "cursed"],
     person: ["mother", "father", "sister", "brother", "grandma", "husband", "wife", "aunt", "uncle", "grandpa", "son", "daughter", "teacher", "student", "college student", "doctor", "police officer", "software engineer", "trick-or-treater", "violin teacher", "ghost", "witch", "wizard", "zombie", "vampie", "werewolf"],
     place: ["school", "workplace", "grocery store", "Safeway", "college", "hospital", "house", "farm", "tech startup", "nuclear power plant", "graveyard", "mausoleum", "spirit world", "jail", "sewer", "forest", "cave", "fortune teller", "corn maze", "secret passage", "underworld"],
     thing: ["carrot", "apple", "orange", "computer", "house", "rotisserie-chicken", "cake", "homework", "portfolio-project", "pumpkin", "potion", "poison apple", "spellbook", "jack-o-lantern", "broom", "cauldron", "tombstone", "eye of newt", "pentagram", "candy corn", "corpse"],
@@ -12,26 +12,26 @@ const dictionary = {
 };
 
 const generatePronoun = (word) => {
-    let pronoun = "they";
+    let pronouns = ["they", "them", "their", "theirs"];
     if (word === "mother" || word === "grandma" || word === "sister" || word === "wife" || word === "aunt" || word === "daughter" || word === "witch") {
-        pronoun = "she";
+        pronouns = ["she", "her", "her", "hers"];
     } else if (word === "father" || word === "brother" || word === "husband" || word === "uncle" || word === "grandpa" || word === "son" || word === "wizard") {
-        pronoun = "he";
+        pronouns = ["he", "him", "his", "his"];
     } else {
         //Other words get a fifty-fifty chance
         let fiftyFifty = (Math.floor(Math.random() * 2) == 1 );
         if (fiftyFifty) {
-            pronoun = "he";
+            pronouns = ["he", "him", "his", "his"];
         } else {
-            pronoun = "she";
+            pronouns = ["she", "her", "her", "hers"];
         }
     }
     //I created a 1 in 10 chance that the person uses a gender-neutral pronoun, regardless of the word
     let neutralChance = (Math.floor(Math.random() * 10) + 1);
     if (neutralChance == 1) {
-        pronoun = "they";
+        pronouns = ["they", "them", "their", "theirs"];
     }
-    return pronoun;
+    return pronouns;
 }
 
 function randomlySelectFromArray(array) {
@@ -74,21 +74,25 @@ const randomAdverb = () => {
     return randomlySelectFromArray(dictionary.adverb);
 }
 
-const getMessage = (person, pronoun) => {
+const getMessage = (person, pronounThird, pronounId, pronounPos, pronounPosId) => {
     let templates = [
-        "I sent my " + person + " to the " + randomPlace() + " to get some " + randomThings() + ". But " + pronoun + " came back with a " + randomThing() + "!",
-        pronoun + " told me I wasn't allowed to " + randomVerb() + " in the " + randomPlace() + ", so I felt " + randomAdjective() + "..."
+        "I sent my " + person + " to the " + randomPlace() + " to get some " + randomThings() + ". But " + pronounThird + " came back with a " + randomThing() + "!",
+        pronounThird + " told me I wasn't allowed to " + randomVerb() + " in the " + randomPlace() + ". I've never felt so " + randomAdjective() +".",
+
+
 ];
     return randomlySelectFromArray(templates);
 }
 
 const generateMessage = () => {
     let person = randomPerson();
-    let pronoun = generatePronoun(person);
-    let message = getMessage(person, pronoun);
-    const capitalized =
-    message.charAt(0).toUpperCase()
-    + message.slice(1);
+    let pronouns = generatePronoun(person);
+    let pronounThird = pronouns[0];
+    let pronounId = pronouns[1];
+    let pronounPos = pronouns[2];
+    let pronounPosId = pronouns[3];
+    let message = getMessage(person, pronounThird, pronounId, pronounPos, pronounPosId);
+    const capitalized = message.charAt(0).toUpperCase() + message.slice(1);
     return capitalized
 }
 
@@ -101,4 +105,4 @@ const generateMessage = () => {
 
 console.log(generateMessage());
 
-console.log("The script has been run! :)");
+console.log("The script has been run successfully! :)");
