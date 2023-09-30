58b421a0fbf1380087aef7b3d7c964f9898b6d4f
Andrew Lipuma <andrewlipuma@gmail.com>
2023-09-29 21:46:16 -0700

    Add helper methods to generate random words, and fixed pronoun support.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const dictionary = {
     adjectives: ["hungry", "sleepy", "ugly", "wonderful", "polite", "handsome", "talented", "awesome", "terrible", "dumb", "silly", "happy", "confused", "sour", "sweet", "salty", "determined", "fearless", "scary", "spooky", "hilarious", "rad", "cool", "beautiful", "haunted", "cursed"],
     person: ["mother", "father", "sister", "brother", "grandma", "husband", "wife", "aunt", "uncle", "grandpa", "son", "daughter", "teacher", "student", "college student", "doctor", "police officer", "software engineer", "trick-or-treater", "violin teacher", "ghost", "witch", "wizard", "zombie", "vampie", "werewolf"],
     place: ["school", "workplace", "grocery store", "Safeway", "college", "hospital", "house", "farm", "tech startup", "nuclear power plant", "graveyard", "mausoleum", "spirit world", "jail", "sewer", "forest", "cave", "fortune teller", "corn maze", "secret passage", "underworld"],
-    thing: ["carrot", "apple", "orange", "computer", "house", "rotisserie-chicken", "cake", "homework", "portfolio-project", "pumpkin", "potion", "poison apple", "spellbook", "jack-o-lantern", "broom", "cauldron", "tombstone", "eye of newt", "pentagram", "candy corn"],
+    thing: ["carrot", "apple", "orange", "computer", "house", "rotisserie-chicken", "cake", "homework", "portfolio-project", "pumpkin", "potion", "poison apple", "spellbook", "jack-o-lantern", "broom", "cauldron", "tombstone", "eye of newt", "pentagram", "candy corn", "corpse"],
     things: ["fruits", "vegetables", "legumes", "eyeballs", "cobwebs", "toenails", "voodoo dolls", "noodles", "flowers", "video games", "clothes", "groceries", "halloween decorations", "autumn leaves", "spiders", "snacks"],
     verb: ["eat", "sleep", "run", "walk", "cry", "laugh", "haunt", "cast a spell", "cackle", "do a ritual"],
     verbed: ["ate", "slept", "ran", "walked", "cried", "laughed", "haunted", "casted a spell", "cackled", "did a ritual"],
@@ -11,7 +11,6 @@ const dictionary = {
     adverb: ["always", "never", "quickly", "slowly", "sometimes", "usually", "rarely"]
 };
 
-//I'm realizing that for this to work well, I would need the message template to be an array so I can index the correct word to generate a pronoun for
 const generatePronoun = (word) => {
     let pronoun = "they";
     if (word === "mother" || word === "grandma" || word === "sister" || word === "wife" || word === "aunt" || word === "daughter" || word === "witch") {
@@ -19,6 +18,7 @@ const generatePronoun = (word) => {
     } else if (word === "father" || word === "brother" || word === "husband" || word === "uncle" || word === "grandpa" || word === "son" || word === "wizard") {
         pronoun = "he";
     } else {
+        //Other words get a fifty-fifty chance
         let fiftyFifty = (Math.floor(Math.random() * 2) == 1 );
         if (fiftyFifty) {
             pronoun = "he";
@@ -34,15 +34,67 @@ const generatePronoun = (word) => {
     return pronoun;
 }
 
-const messageTemplates = [
-    ["I sent my"]
-];
-
 function randomlySelectFromArray(array) {
     return array[Math.floor(Math.random() * array.length)]
 }
 
+const randomAdjective = () => {
+    return randomlySelectFromArray(dictionary.adjectives);
+}
+
+const randomPerson = () => {
+    return randomlySelectFromArray(dictionary.person);
+}
+
+const randomPlace = () => {
+    return randomlySelectFromArray(dictionary.place);
+}
+
+const randomThing = () => {
+    return randomlySelectFromArray(dictionary.thing);
+}
+
+const randomThings = () => {
+    return randomlySelectFromArray(dictionary.things);
+}
+
+const randomVerb = () => {
+    return randomlySelectFromArray(dictionary.verb);
+}
+
+const randomVerbed = () => {
+    return randomlySelectFromArray(dictionary.verbed);
+}
+
+const randomVerbing = () => {
+    return randomlySelectFromArray(dictionary.verbing);
+}
+
+const randomAdverb = () => {
+    return randomlySelectFromArray(dictionary.adverb);
+}
+
+const getMessage = (person, pronoun) => {
+    let templates = [
+        "I sent my " + person + " to the " + randomPlace() + " to get some " + randomThings() + ". But " + pronoun + " came back with a " + randomThing() + "!",
+        pronoun + " told me I wasn't allowed to " + randomVerb() + " in the " + randomPlace() + ", so I felt " + randomAdjective() + "..."
+];
+    return randomlySelectFromArray(templates);
+}
+
+const generateMessage = () => {
+    let person = randomPerson();
+    let pronoun = generatePronoun(person);
+    return getMessage(person, pronoun);
+}
 
 
+
+//Repeater for testing
+// for (let i = 0; i < 20; i++) {
+//     console.log(generatePronoun("teacher"));
+// }
+
+console.log(generateMessage());
 
 console.log("The script has been run! :)");
